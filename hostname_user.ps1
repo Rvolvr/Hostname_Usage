ee2f1093639d331502600c85c7c8a4b801193aa2
Rvolvr <revolverwest@gmail.com>
2021-06-04 09:54:51 -0600

    Revert this script to master
    Start using fork

--- hostname_user.ps1
+++ hostname_user.ps1
@@ -1,6 +1,7 @@
 Import-Module ActiveDirectory
 Import-Module DnsClient
 [array]$export = $null
+#$ErrorActionPreference= 'silentlycontinue'
 
 #Import the list of computers from within a CSV with the label 'name'
 $path = ".\"
@@ -8,21 +9,8 @@ $base = Get-Content "$Path\computernames.csv"
 
 ForEach ($bases in $base){
     #Pull each name for their individual tests, but only enabled machines
-
-    #if the computer is not on the domain, all other tests will fail. Add name to list
-    Try {
-        $ADcomp = Get-ADcomputer $bases | Where-Object {$_.enabled -eq 'True'} | Select-Object -Property Name, DNSHostName, Enabled
-    } Catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] { 
-        write-host "Computer is not on domain, Least risk of using $bases"
-        $base
-        $export += $bases
-    }
-    Try {
-        $ip = Resolve-DnsName $ADcomp | Select-Object -Expandproperty IPAddress
-    } Catch {
-        $_
-    }
-
+    $ADcomp = Get-ADcomputer $bases | Where-Object {$_.enabled -eq 'True'} | Select-Object -Property Name, DNSHostName, Enabled
+    $ip = Resolve-DnsName $ADcomp | Select-Object -Expandproperty IPAddress
     $Ping = test-connection $bases -Quiet
 
 IF ($null -eq $ADComp){
@@ -40,7 +28,7 @@ IF ($null -eq $ADComp){
             
         }else{
             Write-host "$($ADComp.name) has the address of $ip, it's been active recently: continuing to test"
-            $ADcomp | Add-Member -InputObject $ip -Name IPAddress -MemberType NoteProperty
+            $ADcomp | Add-Member -InputObject $ip -Name IPAddress
 
             #this establishes if the computer is accessible throught the network. This is the best test to see if the computer is active
             if ($false -eq $ping){
