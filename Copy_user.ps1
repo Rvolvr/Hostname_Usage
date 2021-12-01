7d3dd1bed6f77c418d97c563955a2afd057829ed
Aaron F <revolverwest@gmail.com>
2021-11-30 17:46:22 -0700

    Minor edits (#17)
    * Formatting edits and minor bug fixes
    
    * Add note space
    
    Add not areas and minor spacing changes
    
    * Quotes and Spacing changes

--- Copy_user.ps1
+++ Copy_user.ps1
@@ -4,10 +4,12 @@ Mirror an existing user for overwriting another user.
 
 .Description
 Generally for an established user that now has a requirement to mirror another user. Commenting out the areas which do not need to be overwritten is a good idea. 
+
+.NOTES
+
 #>
 
-
-#Establish the variables which we will use later: felt it was necessary to force some arrays
+# Establish the variables which we will use later: felt it was necessary to force some arrays
 [Array]$group = $null
 [Array]$toGroupAdd = $null
 [Array]$toGroupDel = $null
@@ -17,46 +19,49 @@ $toAD = $null
 $from = Read-Host "What's the username of the FROM user?"
 $to = Read-Host "What's the username of the TO user?"
 
-#Pulling each of the users in from Active Directory
+# Pulling each of the users in from Active Directory
 $fromAD = Get-ADUser -Filter {samaccountname -like $from} -property description,manager,title,department,company
 $toAD = Get-ADUser -Filter {samaccountname -like $to}
 
 
 
-#Test verifying both users are in AD
+# Test verifying both users are in AD
 if ($null -ne $fromAD) {
     Write-Host "User $($fromAD) is the FROM user"
     if ($null -ne $toAD) {
         Write-Host "User $($toAD) is the FROM user"
 
-        #Pull group memberships for comparison
-        $toPrince = Get-ADPrincipalGroupMembership $toAD | Where-Object name -notlike "Domain Users"
-        $fromPrince = Get-ADPrincipalGroupMembership $fromAD | Where-Object name -notlike "Domain Users"
+### Pull group memberships for comparison
+        $toPrince = Get-ADPrincipalGroupMembership $toAD | Where-Object name -notlike 'Domain Users'
+        $fromPrince = Get-ADPrincipalGroupMembership $fromAD | Where-Object name -notlike 'Domain Users'
 
-        #If a group is in the ORIGINATOR, but is not in the COPY - that group needs to be added 
+### If a group is in the ORIGINATOR, but is not in the COPY - that group needs to be added 
         Foreach ($group in $fromPrince){
             If ($toPrince -notcontains $group){
                 $toGroupAdd += $group
             }    
         }
 
-        #If a group is in the COPY, but is not in ORIGINATOR - that group needs to be removed 
+### If a group is in the COPY, but is not in ORIGINATOR - that group needs to be removed 
         foreach ($group in $toPrince) {
             If ($fromPrince -notcontains $group){
                 $toGroupDel += $group
             }
         }
 
-        #Do the user and group changes verbosely
+### Do the user and group changes verbosely
         $toGroupAdd | ForEach-Object {Add-ADPrincipalGroupMembership -Identity $toAD -MemberOf $_ -Verbose}
         $toGroupDel | ForEach-Object {Remove-ADPrincipalGroupMembership -Identity $toAD -MemberOf $_ -Verbose}
         set-aduser $ToAD -description $fromAD.description -manager $fromAD.manager -title $fromAD.title -department $fromAd.department -company $fromAD.company -Verbose
 
-    } else {
-        #Test 2 failed to find the user
+    } Else {
+
+### Test 2 failed to find the user
         Write-Error "No user $To"
     }
-} else {
-    #Test 1 failed to find the user
+    
+} Else {
+
+## Test 1 failed to find the user
     Write-Error "No user $From"
 }
--- Update_Office.ps1
+++ Update_Office.ps1
@@ -7,23 +7,27 @@ Sets off an update for the "Click to Run" version of Office.
 This enters a PS Session, admin credentials may be required. The PSSession is closed upon exit.
 #>
 
-#Pull list of names from a docuemnt
+# Pull list of names from a docuemnt
 $collection = Get-Content '.\office_list.csv'
 
-#Option to use an admin account
+# Option to use an admin account
 $sesh = Get-Credential
 
 foreach ($item in $collection) {
-    #test to see if computer in list is online
+
+## test to see if computer in list is online
     $online = Test-Connection $item -quiet -count 2
     if ($true -eq $online) {
-        #open a remote session on the named computer and run the update.
+
+### Open a remote session on the named computer and run the update.
         Enter-PSSession -ComputerName $item -Credential $sesh
-        # Here is the work.
+
+### Here is the work.
         Set-Location 'C:\Program Files\Common Files\microsoft shared\ClickToRun\'
         & .\OfficeC2RClient.exe /changesetting Channel=Current
         & .\OfficeC2RClient.exe /update user
-        #This will close the session.
+
+### This will close the session.
         Exit-PSSession
     }
 }
