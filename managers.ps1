bd6acc9c94a40e5d48d8aa64c665d2163c0c1816
Aaron Fleck <revolverwest@gmail.com>
2021-12-02 14:52:41 -0700

    Managers script to hash table

--- managers.ps1
+++ managers.ps1
@@ -4,6 +4,9 @@
 .Description 
     This script will update the user (NT) with the associated manager (Manager) then fill out the organizational information.
 
+.INPUTS
+    No parameters are needed when run.
+
 .NOTES
     Our organization has the employee ID as the login and in the Homepage attribute. 
 
@@ -12,12 +15,19 @@
 Import-module ActiveDirectory
 
 [array]$export = $null
-$list = Import-CSV \manager_roster.csv
+$list = Import-CSV "\manager_roster.csv"
+$Date = Get-Date -UFormat "%Y-%m-%d"
 
 
 
-foreach ($agent in $list){
-    $export += Get-ADUser $agent.NT | Set-ADUser -Manager $agent.managerID -Description $agent.title -title $agent.title -office $agent.country -homepage $agent.NT -department $agent.department 
-    
-}    
-$export | Export-Csv .\agent_move.csv -NoTypeInformation
+foreach ($Employee in $list){
+    $export += Get-ADUser $Employee.NT | Set-ADUser -Verbose @{
+        Manager = $Employee.managerID
+        Description = $Employee.title
+        title = $Employee.title
+        office = $Employee.country
+        homepage = $Employee.NT
+        Department = $Employee.department 
+    }
+}
+$export | Export-Csv ".\Employee_move_$date.csv" -NoTypeInformation
